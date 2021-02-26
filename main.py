9d80655024c83222112d936e73f063a7f44e4dff
stubbs221 <stubbsn8@gmail.com>
2021-02-26 21:46:02 +0300

    main: added basic game script

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-sgdtjdfbdtjdfnbdf
+import numpy as np
+
+
+def game_core_v1(number):
+    count = 0
+    while True:
+        count += 1
+        predict = np.random.randint(1, 101)
+        if number == predict:
+            return count
+
+
+def score_game(game_core):
+    count_ls = []
+    np.random.seed(1)
+    random_array = np.random.randint(1, 101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+
+
+score_game(game_core_v1)
+
